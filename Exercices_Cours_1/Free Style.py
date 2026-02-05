553c67a190290a962f1a1f290d53255501c064d7
MaxenceLt <maxence.letourneau@videotron.ca>
2026-02-05 12:48:46 -0500

    FreeStyle

---
+++ Exercices_Cours_1/Free Style.py
@@ -0,0 +1,6 @@
+Goals=6
+Assists=12
+Games=57
+Différentiel=-3
+B_Shot=124
+print(f"Statistiques 25-26 d'Alexandre Carrier : {Games} Gp, {Goals} G, {Assists} A, {Goals+Assists} Pts, {Différentiel:} (+/-), {B_Shot} Tirs Bloqués")
