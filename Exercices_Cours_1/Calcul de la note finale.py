0283a88f304781255997cc6ad7956f0fbbf495bc
MaxenceLt <maxence.letourneau@videotron.ca>
2026-02-05 14:29:09 -0500

    fini pour aujourd'hui

--- Exercices_Cours_1/Calcul de la note finale.py
+++ Exercices_Cours_1/Calcul de la note finale.py
@@ -1,10 +1,10 @@
-s=input("Saisir un montant : ")
-print("Billets :")
-print(f"{int(float(s)/100)} x 100$")
-print(f"{int(((float(s)%100)/50))} x 50$")
-print(f"{int(float(((float(s)%100)%50))/20)} x 20$")
-print(f"{int(float(float(((float(s)%100)%50))%20)/10)} x 10$")
-print(f"{int(float(float(float(((float(s)%100)%50))%20)%10)/5)} x 5$")
-print(f" Reste{int(float(float(float(((float(s)%100)%50))%20)%10)/5)} x 5$")
+nom_etudiant=(input("Étudiant : "))
+note_tp1=(input("Note TP1 (20% sur 10 pts) : "))
+note_tp2=(input("Note TP2 (30% sur 10 pts) : "))
+examen_1=(input("Note examen #1 (20% sur 100 pts) : "))
+examen_2=(input("Note examen #2 (30% sur 100 pts) : "))
+print(f"Note obtenue pour ce cours : {(((float(note_tp1))*2)+((float(note_tp2))*3)+((float(examen_1))*0.2)+((float((examen_2))*0.3))):.2f}")
+print("Attention, l'application de la double sanction n'a pas été réalisée!")
+
 
 
---
+++ Exercices_Cours_1/Déposer un montant en billet.py
@@ -0,0 +1,11 @@
+s=input("Saisir un montant : ")
+print("Billets :")
+print(f"{int(float(s)/100)} x 100$")
+print(f"{int(((float(s)%100)/50))} x 50$")
+print(f"{int(float(((float(s)%100)%50))/20)} x 20$")
+print(f"{int(float(float(((float(s)%100)%50))%20)/10)} x 10$")
+print(f"{int(float(float(float(((float(s)%100)%50))%20)%10)/5)} x 5$")
+print(f"Reste {float(float(float(((float(s)%100)%50))%20)%10)%5:.2f}$")
+
+
+
