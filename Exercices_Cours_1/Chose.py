d3dbbb9cbd6b21f3341f25c6ca447c3481c2c79f
MaxenceLt <maxence.letourneau@videotron.ca>
2026-02-05 09:11:08 -0500

    Travail

--- Exercices_Cours_1/Chose.py
+++ Exercices_Cours_1/Chose.py
@@ -9,3 +9,17 @@ y = 7
 resultat = x * y
 x = 0
 y = resultat
+
+# Trouver le plus petit
+note1 = 85
+note2 = 72
+note3 = 91
+plus_petite = min(note1, note2, note3)
+print(f"La plus petite note est : {plus_petite}")  # 72
+
+# Trouver le plus grand
+plus_grande = max(note1, note2, note3)
+print(f"La plus grande note est : {plus_grande}")  # 91
+
+masterclass="manger"
+len(masterclass)
